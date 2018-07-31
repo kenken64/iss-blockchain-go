e0bfe8d8186dcc5645f486e0ca40743d05098f3e
Kenneth Phang <bunnyppl@gmail.com>
2018-07-31 12:16:27 +0800

    fix the entire bs

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 
-* Get Golang middleware
+* Get Golang Web middleware - GIN
 ```
 go get github.com/gin-gonic/gin
 ```
 
+Start the app 
+install gomon
+
+```
+gomon blockchain.go
+```
+
+aleternatively 
+
+```
+go run blockchain.go
+```
--- blockchain.go
+++ blockchain.go
@@ -1,14 +1,194 @@
-package blockchain
+package main
 
+/*
+* short variable declaration
+* Appending to a slice
+* func append(s []T, vs ...T) []T
+* Go has pointers. A pointer holds the memory address of a value.
+
+The type *T is a pointer to a T value. Its zero value is nil.
+
+var p *int
+The & operator generates a pointer to its operand.
+
+i := 42
+p = &i
+The * operator denotes the pointer's underlying value.
+
+fmt.Println(*p) // read i through the pointer p
+*p = 21         // set i through the pointer p
+This is known as "dereferencing" or "indirecting".
+
+Unlike C, Go has no pointer arithmetic.
+*/
 import (
+	"crypto/sha256"
 	"fmt"
-	"strconv"
-	"strings"
+	"net/http"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
-package blockchain
+type Block struct {
+	Index      int         `json:"index"`
+	Timestamp  time.Time   `json:"date"`
+	LastHash   string      `json:"prevHash"`
+	Hash       string      `json:"hash"`
+	Data       interface{} `json:"transaction"`
+	nonce      uint32
+	difficulty int
+}
 
-type Blockchain struct {
-	chain	[]*Block `json:"chain"`
+type Blocks []*Block
+
+type Chain interface {
+	AddBlock(block *Block)
+	GetLastBlock() *Block
+	GetBlocks() Blocks
+	IsChainValid() bool
+}
+
+type BlockChain struct {
+	Blocks Blocks
+}
+
+type Transaction struct {
+	From   string  `json:"from"`
+	To     string  `json:"to"`
+	Amount float64 `json:"amount"`
+	Date   time.Time
+}
+
+/*
+* New a block
+* this is to demonstrate pointer
+ */
+func NewBlock(index int, data interface{},
+	timestamp time.Time) *Block {
+	return &Block{
+		Index:     index,
+		Timestamp: timestamp,
+		Data:      data,
+	}
+}
+
+func NewTransaction(from string, to string, amount float64) *Transaction {
+	return &Transaction{
+		From:   from,
+		To:     to,
+		Amount: amount,
+		Date:   time.Now(),
+	}
+}
+
+/*
+*	sprintf is string formatting
+* (*)
+ */
+func (b *Block) createHash() string {
+	d := fmt.Sprintf("%v%v%v%v", b.Index, b.LastHash, b.Timestamp, b.Data)
+	h := sha256.New()
+	h.Write([]byte(d))
+	return fmt.Sprintf("%x", h.Sum(nil))
+}
+
+//RFC3339     = "2006-01-02T15:04:05Z07:00"
+func NewBlockChain() Chain {
+	// time.Now()
+	date, error := time.Parse(time.RFC3339, "2018-08-20T22:08:41+00:00")
+	if error != nil {
+		panic(error)
+	}
+	// create genesis block
+	genesisBlock := NewBlock(0, "Genesis Block", date)
+	genesisBlock.LastHash = "0"
+	genesisBlock.Hash = genesisBlock.createHash()
+
+	var blocks Blocks
+	blocks = append(blocks, genesisBlock)
+
+	return &BlockChain{
+		Blocks: blocks,
+	}
+}
+
+func (b *BlockChain) AddBlock(block *Block) {
+	block.LastHash = b.GetLastBlock().Hash
+
+	// set current hash from created hash
+	block.Hash = block.createHash()
+	b.Blocks = append(b.Blocks, block)
+}
+
+func (b *BlockChain) GetLastBlock() *Block {
+	return b.Blocks[len(b.Blocks)-1]
+}
+
+func (b *BlockChain) GetBlocks() Blocks {
+	return b.Blocks
+}
+
+func (b *BlockChain) IsChainValid() bool {
+	for i := 1; i < len(b.Blocks); i++ {
+		currentBlock := b.Blocks[i]
+		prevBlock := b.Blocks[i-1]
+
+		if currentBlock.Hash != currentBlock.createHash() {
+			return false
+		}
+
+		if currentBlock.LastHash != prevBlock.Hash {
+			return false
+		}
+	}
+
+	return true
+}
+
+func main() {
+	fmt.Println("[ Starting Blockchain API Server ]")
+
+	// Instantiate a new blockchain
+	blockchain := NewBlockChain()
+	r := gin.Default()
+
+	r.GET("/blocks", func(c *gin.Context) {
+		currentBlocks := blockchain.GetBlocks()
+		var interfaceSlice []interface{} = make([]interface{}, len(currentBlocks))
+		for i := range currentBlocks {
+			fmt.Println(currentBlocks[i].Index)
+			fmt.Println(currentBlocks[i].Timestamp)
+			fmt.Println(currentBlocks[i].Hash)
+			fmt.Println(currentBlocks[i].LastHash)
+			fmt.Println(currentBlocks[i].Data)
+			blockJson := &Block{
+				Index:     currentBlocks[i].Index,
+				Timestamp: currentBlocks[i].Timestamp,
+				LastHash:  currentBlocks[i].LastHash,
+				Hash:      currentBlocks[i].Hash,
+				Data:      currentBlocks[i].Data,
+			}
+			interfaceSlice[i] = blockJson
+		}
+		c.JSON(http.StatusOK, gin.H{"blocks": interfaceSlice})
+	})
+
+	r.POST("/pay", func(c *gin.Context) {
+		var incomingTransaction Transaction
+		c.BindJSON(&incomingTransaction)
+		transfer := incomingTransaction
+		lengthOfChain := len(blockchain.GetBlocks())
+		indexblock := lengthOfChain + 1
+		transferBlock := NewBlock(indexblock, transfer, time.Now())
+		blockchain.AddBlock(transferBlock)
+		c.JSON(http.StatusOK, gin.H{"transferedValidity": blockchain.IsChainValid()})
+	})
+
+	r.GET("/is-chain-valid", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"chain-validity": blockchain.IsChainValid()})
+	})
+
+	r.Run("localhost:3005")
+
 }
